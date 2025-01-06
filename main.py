24058adb28032e88bce1e20fd6997ff32139f967
amir_barak <binary.eyes.studio@gmail.com>
2025-01-07 00:02:29 +0200

    added function to count words

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello world')
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def count_unique_chars(text):
+    pass
+
+with open("./books/frankenstein.txt") as f:
+    text = f.read()
+    print(f'word count: {count_words(text)}')
+    
